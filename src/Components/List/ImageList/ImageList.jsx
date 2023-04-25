c003d859ecb7bbde2cb959cb7515ce95d36df668
Alex <alopezbelles@gmail.com>
2023-04-25 23:17:07 +0200

    portfolio page ok finish

--- src/Components/List/ImageList/ImageList.jsx
+++ src/Components/List/ImageList/ImageList.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-import  './ImageList.css';
+import './ImageList.css';
 import jsonData from "../../../Assets/projects.json";
+import { useNavigate } from "react-router-dom";
 
 function ImageList() {
-    
-    return (
-        <div className="image-list-container">
-          {jsonData.map((item, index) => (
-            <div key={index} className="image-container">
-              <img src={item.path_image} alt={item.name} />
-              <p className="nameProject">{item.name}</p>
-            </div>
-          ))}
+  const navigate = useNavigate();
+
+  const handleImageClick = (url) => {
+    window.open(url, "_blank");
+  }
+
+  return (
+    <div className="image-list-container">
+      {jsonData.map((item, index) => (
+        <div key={index} className="image-container" onClick={() => handleImageClick(item.github)}>
+          <img src={item.path_image} alt={item.name} />
+          <p className="nameProject">{item.name}</p>
         </div>
-      );
+      ))}
+    </div>
+  );
 }
 
 export default ImageList;
