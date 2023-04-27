23f090e7edca4d0a14613451fa588bcc4a3a0d8f
Alex <alopezbelles@gmail.com>
2023-04-27 13:54:19 +0200

    working on ABOUT page lyout

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -32,9 +32,9 @@ function Header(){
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-center">
             <Nav.Link href="/">Portfolio</Nav.Link>
-            <Nav.Link href="about">About</Nav.Link>
-            <Nav.Link href="experience">Experience</Nav.Link>
-            <Nav.Link href="contact">Contact</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/experience">Experience</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/Containers/About/About.jsx
+++ src/Containers/About/About.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-
 // BOOTSTRAP
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
+import alexPhoto from "../../Assets/alex-photo.png";
 
 // STYLES AND AESSETS
-import './About.css';
+import "./About.css";
 
-function About(){
-    return(
-        <div>
-            <h1>Soy About</h1>
+function About() {
+  return (
+    <Container className="aboutDesign" fluid>
+      <Col className="col1Design">
+        <Image
+          className="profileImage"
+          src={alexPhoto}
+          alt="Descripción de la imagen"
+        ></Image>
+        <div className="description">
+        <h1>Alejandro López</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, architecto tenetur quas ipsa iusto expedita corrupti, aut earum alias dicta voluptas laboriosam doloribus quasi possimus eius nulla. Quo, dolores natus.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, architecto tenetur quas ipsa iusto expedita corrupti, aut earum alias dicta voluptas laboriosam doloribus quasi possimus eius nulla. Quo, dolores natus.</p>
+
+
         </div>
-    )
+      </Col>
+      <Col className="col2Design">
+        <h1>Work Experience & Education</h1>
+      </Col>
+    </Container>
+  );
 }
 
 export default About;
