d6069337c87e1b0fe89f97d7d3c6a555f8612b66
Alex <alopezbelles@gmail.com>
2023-05-04 18:05:40 +0200

    work in Contact lyout

--- src/Containers/Contact/Contact.jsx
+++ src/Containers/Contact/Contact.jsx
@@ -1,21 +1,44 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-
 // BOOTSTRAP
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
 
 // STYLES AND AESSETS
-import './Contact.css';
+import "./Contact.css";
+import logoGithub from "../../Assets/github-logo.png";
+import logoLinkedin from "../../Assets/linkedin-logo.png";
+import logoMail from "../../Assets/mail-logo.png";
+import logoPhone from "../../Assets/phone-logo.png";
 
-function Contact(){
-    return(
-        <div>
-            <h1>Soy Contact</h1>
-        </div>
-    )
+function Contact() {
+  return (
+    <Container fluid className="contactDesign">
+      <Col className="col1Contact">
+        <h1>Contact with me:</h1>
+      </Col>
+      <Col className="col2Contact">
+        <a href="https://github.com/alopezbelles">
+          <Image className="logoDesign" src={logoGithub}></Image>
+        </a>
+        <a href="https://www.linkedin.com/in/alopezbelles/">
+          <Image className="logoDesign" src={logoLinkedin}></Image>
+        </a>
+        <a href="mailto:alopezbelles@gmail.com">
+          <Image className="logoDesign" src={logoMail}></Image>
+        </a>
+        <a href="tel:+34662583482">
+          <Image className="logoDesign" src={logoPhone}></Image>
+        </a>
+      </Col>
+      <Col className="col3Design">
+        <p className="dataContact">alopezbelles@gmail.com</p>
+        <p className="dataContact">+34 662 583 482</p>
+      </Col>
+    </Container>
+  );
 }
 
 export default Contact;
